5ae93ebb0f86e4153407ca771d6131071a1100d5
Jokerge Jup <valerafrolov507@gmail.com>
2025-09-24 22:16:54 +0300

    i hate this place

--- gagagag/Program.cs
+++ gagagag/Program.cs
@@ -10,6 +10,22 @@ namespace gagagag
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter number N:");
+            int n = int.Parse(Console.ReadLine());
+            if (n <= 0)
+            {
+                Console.WriteLine("Need positive number");
+            }
+            else
+            {
+                int b = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    b += i;
+                }
+                Console.WriteLine($"Amount from 1 to {n} = {b}");
+            }
         }
     }
 }
+// И что с ним не так?
